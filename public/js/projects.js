68f1c15edc90261c73500834afb34a9fc2aff8c3
Don Abney <tdonaldabney@gmail.com>
2016-09-16 10:25:19 -0400

    Beginning the html form for repo input to eventually go get projects for a repo

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  setInterval(refreshRace, 5000);
+  // setInterval(refreshRace, 5000);
+  //
 
-  function refreshRace() {
-    $.ajax({
-      url: "http://localhost:8081/list",
-      crossDomain: true
-    }).then(function(data) {
-      var project = $.parseJSON(data);
-	      $(".project").text(project.projects);
-        console.log(project.projects);
-      });
-  }
 });
+
+function getProjectsList() {
+  $.ajax({
+    url: "http://localhost:8081/list",
+    crossDomain: true
+  }).then(function(data) {
+    var project = $.parseJSON(data);
+      $(".project").text(project.projects);
+      console.log(project.projects);
+    });
+}
